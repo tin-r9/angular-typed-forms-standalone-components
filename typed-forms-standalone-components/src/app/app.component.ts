4e3cf29fba1a101bf0a64431376b4b8def298956
Tincho <martin.rios@mifinanzas.com>
2022-10-17 17:47:57 -0300

    add more cases

--- typed-forms-standalone-components/src/app/app.component.ts
+++ typed-forms-standalone-components/src/app/app.component.ts
@@ -11,7 +11,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, UntypedFormControl, Untype
 export class AppComponent {
 
   form = new FormGroup({
-    email: new FormControl<string | null>(null),
+    email: new FormControl<string | null>('example@mail.com', {nonNullable: true}),
     age: new FormControl<number | null>(null),
   });
 
@@ -21,7 +21,21 @@ export class AppComponent {
   });*/
 
   onSubmit() {
-    if (this.form.value.email)
-    console.log(this.form.value.email.length);
+    if (this.form.value.email) {
+      console.log(this.form.value.email.length);
+    }
+    //console.log(this.form.value.age + 1);
+
+    this.form.reset();
+    console.log(this.form.value.age), 'Edad';
+    console.log(this.form.value.email, 'Email'); //Al no agregar la property {nonNullable: true} por mas que nuestro form control tenga tipo, el mismo puede ser null
+    this.setValues();
+  }
+
+  setValues() {
+    this.form.setValue({
+      email: 'angular@mail.com', 
+      age: 12 // si en este caso eliminamos el tipo especifico veremos como se desarrolla un error al ser null
+    });
   }
 }
